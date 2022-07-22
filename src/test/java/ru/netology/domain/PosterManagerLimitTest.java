735cd1c04c96e36c05459914e2cf66f4c30ea861
Daria Burdakova <d.saharova@inbox.ru>
2022-07-22 18:47:23 +0300

    PosterManager added

---
+++ src/test/java/ru/netology/domain/PosterManagerLimitTest.java
@@ -0,0 +1,74 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PosterManagerLimitTest {
+    PosterManager manager = new PosterManager(5);
+   @Test
+    public void shouldGiveList() {
+
+        manager.save("First");
+        manager.save("Second");
+        manager.save("Third");
+
+        String[] expected = {"First", "Second", "Third"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldGiveZero() {
+        String[] expected = {};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGiveLimit() {
+        manager.save("First");
+        manager.save("Second");
+        manager.save("Third");
+        manager.save("Fourth");
+        manager.save("Fifth");
+
+
+        String[] expected = {"Fifth", "Fourth", "Third", "Second", "First"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldGiveOverLimit() {
+        manager.save("First");
+        manager.save("Second");
+        manager.save("Third");
+        manager.save("Fourth");
+        manager.save("Fifth");
+        manager.save("Sixth");
+        manager.save("Seventh");
+        manager.save("Eighth");
+        manager.save("Ninth");
+        manager.save("Tenth");
+
+        String[] expected = {"Tenth", "Ninth", "Eighth", "Seventh","Sixth"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldUnderLimit() {
+        manager.save("First");
+        manager.save("Second");
+        manager.save("Third");
+        manager.save("Fourth");
+
+
+
+        String[] expected = {"Fourth", "Third", "Second", "First"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
---
+++ src/test/java/ru/netology/domain/PosterManagerTest.java
@@ -0,0 +1,97 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PosterManagerTest {
+    PosterManager manager = new PosterManager();
+    @Test
+
+    public void shouldGiveList() {
+
+        manager.save("First");
+        manager.save("Second");
+        manager.save("Third");
+
+        String[] expected = {"First", "Second", "Third"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldGiveZero() {
+        String[] expected = {};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldGiveOne() {
+        manager.save("First");
+        String[] expected = {"First"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldGiveLimit() {
+        manager.save("First");
+        manager.save("Second");
+        manager.save("Third");
+        manager.save("Fourth");
+        manager.save("Fifth");
+        manager.save("Sixth");
+        manager.save("Seventh");
+        manager.save("Eighth");
+        manager.save("Ninth");
+        manager.save("Tenth");
+
+        String[] expected = {"Tenth", "Ninth", "Eighth", "Seventh","Sixth", "Fifth", "Fourth", "Third", "Second", "First"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGiveOverLimit() {
+        manager.save("First");
+        manager.save("Second");
+        manager.save("Third");
+        manager.save("Fourth");
+        manager.save("Fifth");
+        manager.save("Sixth");
+        manager.save("Seventh");
+        manager.save("Eighth");
+        manager.save("Ninth");
+        manager.save("Tenth");
+        manager.save("Eleventh");
+
+        String[] expected = {"Eleventh", "Tenth", "Ninth", "Eighth", "Seventh","Sixth", "Fifth", "Fourth", "Third", "Second"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldGiveUnderLimit() {
+        manager.save("First");
+        manager.save("Second");
+        manager.save("Third");
+        manager.save("Fourth");
+        manager.save("Fifth");
+        manager.save("Sixth");
+        manager.save("Seventh");
+        manager.save("Eighth");
+        manager.save("Ninth");
+
+
+        String[] expected = {"Ninth", "Eighth", "Seventh","Sixth", "Fifth", "Fourth", "Third", "Second", "First"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    }
+
+
+
+
